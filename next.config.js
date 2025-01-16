c84d0abd2a00952a4781cf09354221071762a252
crakton <redemptionjonathan@outlook.com>
2025-01-16 19:38:40 +0100

    refactor: remove unused API service files and update .gitignore

--- next.config.js
+++ next.config.js
@@ -1,29 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	async rewrites() {
-		return [
-			{
-				source: "/api/:path*",
-				destination:
-					"https://afruna-backend-cmsxg.ondigitalocean.app/api/v1/:path*",
-			},
-		];
-	},
-	// images: {
-	// 	remotePatterns: [
-	// 		{ protocol: 'https', hostname: 'afruna-bucket.nyc3.digitaloceanspaces.com', pathname: '**' },
-	// 		{ protocol: 'https', hostname: 'nyc3.digitaloceanspaces.com', pathname: '**' }
-	// 	]
+	// async rewrites() {
+	// 	return [
+	// 		{
+	// 			source: "/api/:path*",
+	// 			destination:
+	// 				"https://afruna-backend-cmsxg.ondigitalocean.app/api/v1/:path*",
+	// 		},
+	// 	];
 	// },
-	// images: {
-	// 	domains: [
-	// 		"afruna-bucket.nyc3.digitaloceanspaces.com",
-	// 		"lh3.googleusercontent.com",
-	// 		// Add other allowed domains here if needed
-	// 	],
-	// },
+
 	images: {
-		domains: ["afruna-bucket.nyc3.digitaloceanspaces.com", 'nyc3.digitaloceanspaces.com', 'https', "lh3.googleusercontent.com", 'afruna-bucket.nyc3.digitaloceanspaces.com', 'nyc3.digitaloceanspaces.com/afruna-bucket/1701417858481'],
+		domains: [],
 	},
 };
 
