dcf13ce3d6d4905a9c3f4802c6a55694282bfbbe
crakton <redemptionjonathan@outlook.com>
2025-01-24 15:00:47 +0100

    feat: enable ESLint to ignore errors during builds in Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 	env: {
 		APPWRITE_ENDPOINT: process.env.APPWRITE_ENDPOINT,
 		APPWRITE_PROJECT_ID: process.env.APPWRITE_PROJECT_ID,
