3fb96d81591e820e23f74b8e70a357ce256b2941
crakton <redemptionjonathan@outlook.com>
2025-02-23 23:19:11 +0100

    feat: add Alert, Table, and Input components; refactor ModalContext to use withReduxProvider for improved state management

--- src/components/context/ModalContext.tsx
+++ src/components/context/ModalContext.tsx
@@ -1,45 +1,45 @@
-'use client'
+"use client";
 
+import withReduxProvider from "@/lib/withReduxProvider";
 import { FC, ReactNode, createContext, useContext, useState } from "react";
+import { ToastProvider } from "react-toast-plus";
 
 interface ModalProviderProps {
-    children: ReactNode;
+	children: ReactNode;
 }
 
 interface ModalContextType {
-  isOpen: boolean;
-  openModal: () => void;
-  closeModal: () => void;
+	isOpen: boolean;
+	openModal: () => void;
+	closeModal: () => void;
 }
 
-const ModalContext = createContext<ModalContextType | undefined>(undefined);
- 
+const WrapperContext = createContext<ModalContextType | undefined>(undefined);
+
 const ModalProvider: FC<ModalProviderProps> = ({ children }) => {
-    const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-    const openModal = () => setIsOpen(true);
-    const closeModal = () => setIsOpen(false);
+	const openModal = () => setIsOpen(true);
+	const closeModal = () => setIsOpen(false);
 
-    const contextValue = {
-      isOpen,
-      openModal,
-      closeModal,
-    };
-    return ( 
-        <>
-          <ModalContext.Provider value={contextValue}>
-            {children}
-          </ModalContext.Provider>
-        </>
-    );
-}
- 
-export default ModalProvider;
+	const contextValue = {
+		isOpen,
+		openModal,
+		closeModal,
+	};
+	return (
+		<WrapperContext.Provider value={contextValue}>
+			{children}
+		</WrapperContext.Provider>
+	);
+};
+
+export default withReduxProvider(ModalProvider);
 
 export function useModal() {
-  const context = useContext(ModalContext);
-  if (!context) {
-    throw new Error('useModal must be used within a ModalProvider');
-  }
-  return context;
+	const context = useContext(WrapperContext);
+	if (!context) {
+		throw new Error("useModal must be used within a ModalProvider");
+	}
+	return context;
 }
